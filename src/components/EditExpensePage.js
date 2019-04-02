4fa6123ad501a562a55a8240de58e80dbba902d2
bala92x <bala92x@gmail.com>
2019-04-02 05:03:07 +0200

    Add id param to edit route

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const EditExpensePage = () => (
-    <div>Edit Expense</div>
+const EditExpensePage = (props) => (
+    <div>Edit Expense with id: {props.match.params.id}</div>
 )
 
 export default EditExpensePage
