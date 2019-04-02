cba1094fe08375293d568db2e3722d3e4e6d6efc
bala92x <bala92x@gmail.com>
2019-04-02 04:20:55 +0200

    Add routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+const ExpenseDashboardPage = () => (
+    <div>Dashboard</div>
+)
+
+const AddExpensePage = () => (
+    <div>Add Expense</div>
+)
+
+const EditExpensePage = () => (
+    <div>Edit Expense</div>
+)
+
+const HelpPage = () => (
+    <div>Help</div>
+)
 
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-ReactDOM.render(<p>React Boilerplate</p>, document.getElementById('app'))
+const routes = (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact component={ExpenseDashboardPage} />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+        </Switch>
+    </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'))
