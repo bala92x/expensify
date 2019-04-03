15643331545d6d0261a76ca2800613cd1e86eb81
Budavölgyi Bálint <bala92x@gmail.com>
2019-04-03 14:20:50 +0200

    Mock moment and add tests for ExpenseForm

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import 'normalize.css/normalize.css'
+import 'react-dates/lib/css/_datepicker.css'
 
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
