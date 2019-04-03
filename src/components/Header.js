ebe69d45b4e29f3448a5cfb6fab24d3c4610c0dc
Budavölgyi Bálint <bala92x@gmail.com>
2019-04-03 11:45:04 +0200

    Add snapshot test to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ const Header = () => (
         <nav>
             <NavLink to="/" activeClassName="is-active" exact >Dashboard</NavLink>
             <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-            <NavLink to="/help" activeClassName="is-active">Help</NavLink>
         </nav>
     </header>
 )
