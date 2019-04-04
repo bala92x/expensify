9ec1cf64260e9272dcf2ddae6e30038e56860bfc
Budavölgyi Bálint <bala92x@gmail.com>
2019-04-04 15:43:06 +0200

    Remove demo data imports

--- src/app.js
+++ src/app.js
@@ -6,8 +6,6 @@ import 'react-dates/lib/css/_datepicker.css'
 
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
-import { addExpense } from './actions/expensesActions'
-import { setTextFilter } from './actions/filtersActions'
 import './styles/styles.scss'
 
 const store = configureStore()
