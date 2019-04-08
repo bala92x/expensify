12768b468f9495f3e26e868890e81e5833fae7b8
Budavölgyi Bálint <bala92x@gmail.com>
2019-04-08 11:19:47 +0200

    Add separate database for tests

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+const webpack = require('webpack')
 const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const publicPath = path.join(__dirname, 'public')
+const NODE_ENV = process.env.NODE_ENV || 'development'
+
+if (NODE_ENV === 'test') {
+    require('dotenv').config({ path: '.env.test' })
+} else if (NODE_ENV === 'development') {
+    require('dotenv').config({ path: '.env.dev' })
+}
 
 module.exports = (env) => {
     const isProduction = env === 'production'
@@ -42,7 +50,15 @@ module.exports = (env) => {
             ]
         },
         plugins: [
-            CSSExtract
+            CSSExtract,
+            new webpack.DefinePlugin({
+                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+            })
         ],
         mode: 'development',
         devtool: isProduction ? 'source-map' : 'inline-source-map',
