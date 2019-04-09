a1d2ca7a2330e1d1e70c0faf903e3d77b931f4d5
Budavölgyi Bálint <bala92x@gmail.com>
2019-04-09 13:53:06 +0200

    Add functioning logout button

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -3,7 +3,15 @@ import { firebase, googleAuthProvider } from '../firebase/firebase'
 export const startLogin = () => {
     return () => {
         return firebase
-                .auth()
-                .signInWithPopup(googleAuthProvider)
+            .auth()
+            .signInWithPopup(googleAuthProvider)
+    }
+}
+
+export const startLogout = () => {
+    return () => {
+        return firebase
+            .auth()
+            .signOut()
     }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const Header = () => (
+import { startLogout } from '../actions/authActions'
+
+export const Header = ({ startLogout }) => (
     <header>
         <h1>Expensify</h1>
         <nav>
             <NavLink to="/dashboard" activeClassName="is-active" exact >Dashboard</NavLink>
             <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+            <button onClick={startLogout}>Logout</button>
         </nav>
     </header>
 )
 
-export default Header
+const mapDispatchToProps = (dispatch) => ({
+    startLogout: () => dispatch(startLogout())
+})
+
+export default connect(undefined, mapDispatchToProps)(Header)
